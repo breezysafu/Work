251bc55ecae6027728d2ae1a759f8604515336b7
breezysafu <34032700+breezysafu@users.noreply.github.com>
2018-11-24 18:02:25 +0545

    Add files via upload

---
+++ ObstacleDetectionCar/servoultrasensor/servoultrasensor.ino
@@ -0,0 +1,34 @@
+
+int trigpin=13; //sensor trig pin is connected to arduino pin 13
+int echopin=11; //sensor echo pin is conected to arduino pin 11
+
+float pingtime;
+float targetdistance=6;
+float speedofsound;
+
+void setup() {
+ Serial.begin(9600); //turn on serial port
+ pinMode(trigpin, OUTPUT);
+ pinMode(echopin, OUTPUT);
+ 
+}
+
+void loop() {
+  digitalWrite(trigpin,LOW);
+  delayMicroseconds(2000);
+  digitalWrite(trigpin,HIGH);
+  delayMicroseconds(15);
+  digitalWrite(trigpin,LOW);
+
+  pingtime=pulseIn(echopin,HIGH);
+
+  speedofsound=2*targetdistance/pingtime;
+  speedofsound=speedofsound/63360*1000000*3600;
+  
+
+  Serial.print("The speed of sound is:");
+  Serial.print(speedofsound);
+  Serial.println("miles per hour");
+
+ 
+ }
